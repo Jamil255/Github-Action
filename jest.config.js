58ccf3513a184201d1304f175bd95824b89e8600
JAMIL AFZAL <134855468+Jamil255@users.noreply.github.com>
2025-10-23 05:25:39 +0500

    Fix moduleNameMapper to retain path in jest config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ export default {
   testEnvironment: 'node',
   transform: {},
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transformIgnorePatterns: ['node_modules/(?!(supertest)/)'],
 }
